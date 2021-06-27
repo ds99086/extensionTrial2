a1b527d8ecec42b6a3f6f537c762aeff14c0d296
ds99086 <ds99086@gmail.com>
2021-06-27 16:58:56 +1200

    better categories but still need hardcoding. pasting current clipboard needs working

--- popup.js
+++ popup.js
@@ -1,22 +1,55 @@
 document.addEventListener('DOMContentLoaded', function (){
-    const textbox = document.getElementById("selectedinput");
+    const textBox = document.getElementById("selectedinput");
+    const clipBox = document.getElementById("clipbox");
+    const optionBox = document.getElementsByClassName("charoptions")[0];
+    
     document.addEventListener('click', function(e) {
 
         if (e.target && e.target.matches(".char")) {
             const textToCopy = e.target.innerHTML;
-            console.log(textToCopy);
-            console.log(textbox);
-            textbox.value += textToCopy;
+            // console.log(textToCopy);
+            // console.log(textBox);
+            textBox.value += textToCopy;
         }
         if (e.target && e.target.matches(".copyinput")){
-            textbox.select();
+            textBox.select();
             document.execCommand("copy");
         }
         if (e.target && e.target.matches(".clearinput")) {
-            textbox.value = "";
+            textBox.value = "";
         }
-    } )
 
+        if (e.target && e.target.matches(".clipstorage")) {
+            //console.log(clipBox);
+            clipBox.select();
+            document.execCommand("paste");
+            const clipText = navigator.clipboard.read();
+            clipBox.innerHTML = clipText; 
+            //console.log(clipBox);
+        }
+    })
 
+    //console.log(optionBox);
 
+    data.forEach(type => {
+
+        let categoryDiv = document.createElement("div");
+        let categoryName = document.createElement("h4");
+        categoryDiv.classList.add("symbol-category");
+        categoryDiv.setAttribute("name", type.name);
+        categoryName.innerHTML = type.name;
+
+        categoryDiv.append(categoryName);
+        optionBox.append(categoryDiv);
+
+        type.symbols.forEach(symbol => {
+            //console.log(symbol);
+            console.log(symbol.value);
+            categoryDiv.innerHTML += `
+            <button class=char>${symbol.value}</button>
+            `
+        });
+    });
+    
 })
+
---
+++ symbolsdb.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        "name": "superscript",
+        "symbols": [
+            {
+                "charname": "superscript-2",
+                "value": "²"
+            },
+            {
+                "charname": "superscript-3",
+                "value": "³"
+            }
+        ]
+    },
+    {
+        "name": "subscript",
+        "symbols": [
+            {
+                "charname": "subscript-2",
+                "value": "₂"
+            },
+            {
+                "charname": "subscript-3",
+                "value": "₃"
+            }
+        ]
+    }
+];
