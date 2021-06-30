d2cadee281d2007aa09a9f79727cbc968a2552ad
ds99086 <ds99086@gmail.com>
2021-06-30 16:29:26 +1200

    displays symbols in categories. updated symbol list. drop down box written.

--- symbolsdb.js
+++ symbolsdb.js
@@ -1,7 +1,15 @@
 const data = [
     {
-        "name": "superscript",
+        "name": "Superscript",
         "symbols": [
+            {
+                "charname": "superscript-0",
+                "value": "⁰"
+            },
+            {
+                "charname": "superscript-1",
+                "value": "¹"
+            },
             {
                 "charname": "superscript-2",
                 "value": "²"
@@ -9,12 +17,84 @@ const data = [
             {
                 "charname": "superscript-3",
                 "value": "³"
+            },
+            {
+                "charname": "superscript-4",
+                "value": "⁴"
+            },
+            {
+                "charname": "superscript-5",
+                "value": "⁵"
+            },
+            {
+                "charname": "superscript-6",
+                "value": "⁶"
+            },
+            {
+                "charname": "superscript-7",
+                "value": "⁷"
+            },
+            {
+                "charname": "superscript-8",
+                "value": "⁸"
+            },
+            {
+                "charname": "superscript-9",
+                "value": "⁹"
+            },
+            {
+                "charname": "superscript-+",
+                "value": "⁺"
+            },
+            {
+                "charname": "superscript--",
+                "value": "⁻"
+            },
+            {
+                "charname": "superscript-=",
+                "value": "⁼"
+            },
+            {
+                "charname": "superscript-(",
+                "value": "⁽"
+            },
+            {
+                "charname": "superscript-)",
+                "value": "⁾"
+            },
+            {
+                "charname": "superscript-a",
+                "value": "ᵃ"
+            },
+            {
+                "charname": "superscript-b",
+                "value": "ᵇ"
+            },
+            {
+                "charname": "superscript-e",
+                "value": "ᵉ"
+            },
+            {
+                "charname": "superscript-x",
+                "value": "ˣ"
+            },
+            {
+                "charname": "superscript-y",
+                "value": "ʸ"
             }
         ]
     },
     {
-        "name": "subscript",
+        "name": "Subscript",
         "symbols": [
+            {
+                "charname": "subscript-0",
+                "value": "₀"
+            },
+            {
+                "charname": "subscript-1",
+                "value": "₁"
+            },
             {
                 "charname": "subscript-2",
                 "value": "₂"
@@ -22,6 +102,128 @@ const data = [
             {
                 "charname": "subscript-3",
                 "value": "₃"
+            },
+            {
+                "charname": "subscript-4",
+                "value": "₄"
+            },
+            {
+                "charname": "subscript-5",
+                "value": "₅"
+            },
+            {
+                "charname": "subscript-6",
+                "value": "₆"
+            },
+            {
+                "charname": "subscript-7",
+                "value": "₇"
+            },
+            {
+                "charname": "subscript-8",
+                "value": "₈"
+            },
+            {
+                "charname": "subscript-9",
+                "value": "₉"
+            },
+            {
+                "charname": "subscript-+",
+                "value": "₊"
+            },
+            {
+                "charname": "subscript--",
+                "value": "₋"
+            },
+            {
+                "charname": "subscript-=",
+                "value": "₌"
+            },
+            {
+                "charname": "subscript-(",
+                "value": "₍"
+            },
+            {
+                "charname": "subscript-)",
+                "value": "₎"
+            },
+            {
+                "charname": "subscript-a",
+                "value": "ₐ"
+            },
+            {
+                "charname": "subscript-e",
+                "value": "ₑ"
+            },
+            {
+                "charname": "subscript-x",
+                "value": "ₓ"
+            }
+        ]
+    },
+    {
+        "name": "Mathematical Signs",
+        "symbols": [
+            {
+                "charname": "plus minus",
+                "value": "±"
+            },
+            {
+                "charname": "division",
+                "value": "÷"
+            },
+            {
+                "charname": "not equal to",
+                "value": "≠"
+            },
+            {
+                "charname": "square root",
+                "value": "√"
+            },
+            {
+                "charname": "cube root",
+                "value": "∛"
+            },
+            {
+                "charname": "less than or equal to",
+                "value": "≤"
+            },
+            {
+                "charname": "greater than or equal to",
+                "value": "≥"
+            },
+            {
+                "charname": "degrees",
+                "value": "°"
+            },
+            {
+                "charname": "infinity",
+                "value": "∞"
+            },
+            {
+                "charname": "angle",
+                "value": "∠"
+            },
+            {
+                "charname": "therefore",
+                "value": "∴"
+            }
+        ]
+    },
+    {
+        "name": "Currency",
+        "symbols": [
+            {
+                "charname": "cent",
+                "value": "¢"
+            },
+            {
+                "charname": "pound",
+                "value": "£"
+            },
+            {
+                "charname": "euro",
+                "value": "€"
             }
         ]
     }
