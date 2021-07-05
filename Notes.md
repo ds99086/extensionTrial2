31acd84d6c31fcaca1a0cbe49284e53deeef8c58
ds99086 <ds99086@gmail.com>
2021-07-05 18:59:26 +1200

    Minimum viable clipboard saving

--- Notes.md
+++ Notes.md
@@ -4,3 +4,5 @@ Note to self
 2. Symbol Package would change display box to that charoption?
 3. Save the clips
 4. Immediately paste if text cursor is activated. - but cursor is lost when extension is clicked. 
+5. add favourite option
+6. recent - make a recent list and display
--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', async function () {
     const textBox = document.getElementById("selectedinput");
-    const clipBox = document.getElementById("clipbox");
+    const clipBox = document.getElementById("clearboard");
     const optionBox = document.getElementsByClassName("charoptions")[0];
 
-    document.addEventListener('click', function (e) {
+    document.addEventListener('click', async function (e) {
 
         if (e.target && e.target.matches(".char")) {
             const textToCopy = e.target.innerHTML;
@@ -19,16 +19,53 @@ document.addEventListener('DOMContentLoaded', function () {
             textBox.value = "";
         }
 
-        if (e.target && e.target.matches(".clipstorage")) {
-            //console.log(clipBox);
+        if (e.target && e.target.matches(".clipbutton")) {
             clipBox.select();
-            document.execCommand("paste");
-            const clipText = navigator.clipboard.read();
-            clipBox.innerHTML = clipText;
+            document.execCommand("copy");
+            // console.log(clipBox);
+            // //console.log(navigator.clipboard.readText());
+            // const response = await readClipboard();
+            // console.log(response);
+            // clipBox.innerHTML = response;
+            //navigator.clipboard.readText().then(text => clipBox.innerText = text);
+            
+            // clipBox.select();
+            // document.execCommand("paste");
+            // const clipText = navigator.clipboard.read();
+            // clipBox.innerHTML = clipText;
             //console.log(clipBox);
         }
     })
 
+    //  async function readClipboard() {
+    //     const clearboard = document.getElementById("clearboard");
+    //     // if (navigator.clipboard) {
+    //     //     
+        
+    //     try {
+    //         console.log(clearboard);
+
+    //         const copied = await navigator.clipboard.read();
+    //         console.log(copied);
+
+
+    //         // clearboard.focus();
+    //         // clearboard.select();
+    //         // clearboard.setSelectionRange(0, 99999);
+    //         // document.execCommand('paste');
+    //         // console.log(document.execCommand('paste'))
+            
+    //         // const content = clearboard.value;
+    //         // console.log("i' got content");
+            
+    //         // console.log(content);
+    //         return content;
+    //     } catch (err) {
+    //         console.error('failed to read clipboard');
+    //         console.error(err);
+    //     }
+    // //}
+    // }
     //console.log(optionBox);
 
     data.forEach(type => {
